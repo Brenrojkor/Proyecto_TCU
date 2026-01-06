c5f6f828fbd770444307c142ac72fe3eb059463d
Brenda Rojas <145055735+Brenrojkor@users.noreply.github.com>
2026-01-06 14:41:21 -0600

    Nav Inicial
    Creación inicial de lo que sería posiblemente el Nav de la app

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -1,8 +1,45 @@
 import flet as ft
 from model.database import Database
 
+
 def main(page: ft.Page):
     page.title = "Biblioteca - Flet"
+    page.appbar = ft.AppBar(
+    title=ft.Text("📚 Biblioteca"),
+    center_title=False,
+    bgcolor=ft.Colors.BLUE_600,
+    actions=[
+    ft.TextButton(
+        content=ft.Row(
+            controls=[ft.Icon(ft.Icons.HOME), ft.Text("Inicio")],
+            spacing=5
+        ),
+        tooltip="Inicio"
+    ),
+    ft.TextButton(
+        content=ft.Row(
+            controls=[ft.Icon(ft.Icons.BOOK), ft.Text("Libros")],
+            spacing=5
+        ),
+        tooltip="Libros"
+    ),
+    ft.TextButton(
+        content=ft.Row(
+            controls=[ft.Icon(ft.Icons.PERSON), ft.Text("Autores")],
+            spacing=5
+        ),
+        tooltip="Autores"
+    ),
+    ft.TextButton(
+        content=ft.Row(
+            controls=[ft.Icon(ft.Icons.LOGOUT), ft.Text("Salir")],
+            spacing=5
+        ),
+        tooltip="Salir"
+    ),
+],    leading=ft.IconButton(ft.Icons.MENU, tooltip="Menú")
+)
+
 
     db = Database()
 
@@ -42,7 +79,26 @@ def main(page: ft.Page):
 
     button_show_libros.on_click = mostrar_libros
 
-    page.add(button_show_libros, libros_table)
+    
+
+    container = ft.Container(
+        content=ft.Column(controls=[button_show_libros, libros_table], spacing=20),
+        padding=20,
+        bgcolor="white",
+        border_radius=8
+        
+    )
+
+ 
+
+
+    page.add(
+    ft.Row(
+        controls=[container],
+        alignment=ft.MainAxisAlignment.CENTER,
+        expand=True
+    )
+)
 
 #Ejecutar la app
 ft.app(target=main)
